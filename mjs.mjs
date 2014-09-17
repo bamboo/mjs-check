a8b1d33c56e7e140662373239e6dc84a8b052410
Rodrigo B. de Oliveira <rbo@acm.org>
2014-09-17 01:12:05 -0300

    ability to check file contents against a different file name

--- mjs.mjs
+++ mjs.mjs
@@ -5,6 +5,7 @@
 #require
   meta-script
   mori
+  fs
 
 fun format-location {line, column} ->
   if line
@@ -19,17 +20,32 @@ fun log-error (file, location, message) ->
   console.log
     file + format-location location + ': ' + message
 
+fun check-file-as (filename, file, mjs) -> do!
+  try
+    var compiler = mjs.compiler-from-string
+      fs.read-file-sync (file, {encoding: 'utf8'})
+      filename
+    var ast = compiler.produce-ast ()
+    compiler.errors.for-each fun error ->
+      log-error (file, error, error.message)
+  catch var e
+    log-error (file, {line: 1, column: 1}, (e.message || e.to-string ()))
+
 fun check files ->
   var mjs = meta-script ()
-  files.for-each fun file -> do!
-    try
-      var compiler = mjs.compiler-from-file file
-      var ast = compiler.produce-ast ()
-      compiler.errors.for-each fun error ->
-        log-error (file, error, error.message)
-    catch var e
-      log-error (file, {line: 1, column: 1}, (e.message || e.to-string ()))
-
+  var filename-stack = []
+  var parsing-file-name = false
+  files.for-each file -> do!
+    if parsing-file-name
+      filename-stack.push file
+      parsing-file-name = false
+    else if (file == '--name')
+      parsing-file-name = true
+    else
+      check-file-as
+        filename-stack.pop () || file
+        file
+        mjs
   0
 
 #external module.exports = check
